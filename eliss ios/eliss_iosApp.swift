0fa36aa056d15daa355809e9ecaad7871c3c25ba
darrelhong <darrel.hong@gmail.com>
2023-04-14 16:52:44 +0800

    Initial Commit

---
+++ eliss ios/eliss_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  eliss_iosApp.swift
+//  eliss ios
+//
+//  Created by darrelhong on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct eliss_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
